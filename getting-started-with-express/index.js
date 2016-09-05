9b6ea2425eea30cba1cddc606d1cb00fbee7e9db
Helton Carlos de Souza <helton.development@gmail.com>
2016-09-04 23:17:29 -0300

    1st lesson - Up and Running

---
+++ getting-started-with-express/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello, World!');
+});
+
+app.get('/yo', function (req, res) {
+  res.send('YO');
+});
+
+var server = app.listen(3000, function () {
+  console.log('Server running at http://localhost:' + server.address().port);
+});
