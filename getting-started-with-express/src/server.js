5263f698671555f966b0850d2961d4ab2c01ddca
Helton Carlos de Souza <helton.development@gmail.com>
2016-09-04 23:24:12 -0300

    2nd lesson - Routing Basics

--- getting-started-with-express/src/server.js
+++ getting-started-with-express/src/server.js
@@ -1,12 +1,40 @@
+const fs = require('fs');
+const _ = require('lodash');
 const express = require('express');
 const app = express();
 
+let users = [];
+
+fs.readFile('users.json', {encoding: 'utf-8'}, (err, data) => {
+  if (err) throw err;
+
+  JSON.parse(data).forEach(user => {
+    user.name.full = _.startCase(user.name.first + ' ' + user.name.last);
+    users.push(user);
+  });
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  var buffer = '';
+  users.forEach(user => {
+    buffer += '<a href="/' + user.username + '">' + user.name.full + '</a> <br />';
+  });
+  res.send(buffer);
 });
 
-app.get('/yo', (req, res) => {
-  res.send('YO');
+app.get(/big.*/, (req, res, next) => {
+  console.log('BIG USER ACCESS');
+  next();
+});
+
+app.get(/.*dog.*/, (req, res, next) => {
+  console.log('DOGS GO WOOF');
+  next();
+});
+
+app.get('/:username', (req, res) => {
+  var username = req.params.username;
+  res.send(username);
 });
 
 const server = app.listen(3000, () => {
